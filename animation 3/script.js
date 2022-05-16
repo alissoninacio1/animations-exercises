76c1e4fb3d17aa68f700eb174c9aa530b0fd3f68
Álisson Inácio <69402669+alissoninacio1@users.noreply.github.com>
2022-05-16 15:13:20 -0600

    Animation and transitions with advanced css 🐱‍👤
    Advanced concepts of animation and transition, with geometric forms.

---
+++ animation 3/script.js
@@ -0,0 +1,7 @@
+// I can select an element direclty using the id
+ativar.addEventListener('click', () => {
+    modal.classList.toggle('animar')   
+})
+
+//togle is like an on/off button - there is no need of using a button to on and another to off 
+//or a class o remove and a class to active
